cd7ec3730b89e81d3f3b735247760750716f4f25
DarthHamza <hamzamakia@gmail.com>
2018-03-27 15:54:45 +0300

    cors setup

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
 
     'my_messages',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,6 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
