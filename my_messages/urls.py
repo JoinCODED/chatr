b08662b93862359f9bd8e0869ce077cc88f407c3
Octowl <alsaff1987@gmail.com>
2018-04-15 14:20:24 +0300

    Rename message create to 'send'

--- my_messages/urls.py
+++ my_messages/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', ChannelListAPIView.as_view(), name='channel-list'),
     path('create/', ChannelCreateAPIView.as_view(), name='channel-create'),
     path('<int:channel_id>/', MessageListView.as_view(), name='message-list'),
-    path('<int:channel_id>/create/', MessageCreateView.as_view(), name='message-create'),
+    path('<int:channel_id>/send/', MessageCreateView.as_view(), name='message-create'),
 ]
