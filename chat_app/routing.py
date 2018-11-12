77850acde4f8df478a84a9feea768ab160810f17
Octowl <alsaff1987@gmail.com>
2018-11-12 09:49:18 +0300

    Add consumer and routing

--- chat_app/routing.py
+++ chat_app/routing.py
@@ -1,5 +1,13 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import my_messages.routing
+
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
+    # Empty for now (http->django views is added by default)\
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            my_messages.routing.websocket_urlpatterns
+        )
+    ),
 })
